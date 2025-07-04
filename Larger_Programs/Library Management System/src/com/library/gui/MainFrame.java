57fb7fe78a46f105d9160f16bf6b23c414364434
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-07-04 20:06:45 +0300

    Add files via upload

---
+++ Larger_Programs/Library Management System/src/com/library/gui/MainFrame.java
@@ -0,0 +1,11 @@
+package com.library.gui;
+
+import java.io.Serializable;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame implements Serializable {
+
+	private static final long SerialVersionUID = 1L;
+	
+}
