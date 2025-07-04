128844b067265880b2f4ddf40dcfa6f3210ec272
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-07-05 00:24:58 +0300

    Add files via upload

---
+++ Larger_Programs/Library Management System/src/com/library/gui/LoginFrame.java
@@ -0,0 +1,17 @@
+package com.library.gui;
+
+import java.io.Serializable;
+
+import javax.swing.JFrame;
+
+public class LoginFrame extends JFrame implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	public LoginFrame() {
+		this.setSize(WIDTH, HEIGHT);
+	}
+
+}
