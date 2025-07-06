9ba37829681f92bbae9b7a93e0993b8c2276ba9e
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-07-06 23:17:49 +0300

    Add files via upload

---
+++ Larger_Programs/Library Management System/src/com/library/gui/custom/AdvancedCustomLabel.java
@@ -0,0 +1,15 @@
+package com.library.gui.custom;
+
+import java.io.Serializable;
+
+import javax.swing.JLabel;
+
+public class AdvancedCustomLabel extends JLabel implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	// Animation and visual properties for the label
+	private float shadowOpacity = 0.3f;
+	private int shadowOffsetX = 2;
+
+}
