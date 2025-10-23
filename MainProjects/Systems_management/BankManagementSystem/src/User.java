af45c5ce2ff35dcd3bbee1388b18f306a28a3268
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-10-23 18:08:16 +0300

    Add files via upload

---
+++ MainProjects/Systems_management/BankManagementSystem/src/User.java
@@ -0,0 +1,106 @@
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime registrationDate;
+    private List<BankAccount> accounts;
+    private boolean isAdmin;
+
+    public User(String username, String password, String fullName, String email, String phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.registrationDate = LocalDateTime.now();
+        this.accounts = new ArrayList<>();
+        this.isAdmin = false;
+    }
+
+    public void addAccount(BankAccount account) {
+        accounts.add(account);
+    }
+
+    public void removeAccount(BankAccount account) {
+        accounts.remove(account);
+    }
+
+    public BankAccount getAccountByNumber(String accountNumber) {
+        for (BankAccount account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public List<BankAccount> getAccounts() {
+        return new ArrayList<>(accounts);
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public double getTotalBalance() {
+        double total = 0;
+        for (BankAccount account : accounts) {
+            total += account.getBalance();
+        }
+        return total;
+    }
+}
