5995a5619a54eee5422d4c91f567973e48d5f53b
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-01-26 04:59:32 +0200

    Add files via upload

---
+++ Practice/problemsolving/SwapNums.java
@@ -0,0 +1,20 @@
+package com.tozu.problemsolving;
+
+public class SwapNums {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		int y = 6;
+		
+		System.out.println("X: " + x + " Y: " + y);
+		
+		int temp = x;
+		x = y;
+		y = temp;
+		
+		System.out.println("X: " + x + " Y: " + y);
+
+	}
+
+}
